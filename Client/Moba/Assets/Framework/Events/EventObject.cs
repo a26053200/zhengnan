df0c4112d54485d5173513814e8fa25541600100
a26053200 <zhengnan262@qq.com>
2018-05-23 02:07:01 +0800

    提交客户端代码

---
+++ Client/Moba/Assets/Framework/Events/EventObject.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+/**
+ * EventObject 类作为创建 Event 对象的基类，当发生事件时，Event 对象将作为参数传递给事件侦听器。 
+**/
+public class EventObject
+{
+    public const String ACTIVATE = "activate";
+    public const String ADDED = "added";
+    public const String ADDED_TO_STAGE = "addedToStage";
+    public const String CANCEL = "cancel";
+    public const String CHANGE = "change";
+    public const String CLEAR = "clear";
+    public const String CLOSE = "close";
+    public const String CLOSING = "closing";
+    public const String COMPLETE = "complete";
+    public const String CONNECT = "connect";
+    public const String OPEN = "open";
+
+    public const String DISCONNECT = "disconnect";
+    public const String SERVER_CONNECTED = "server connected";
+    private EventTrigger _target;
+    private int _eventPhase;
+    private EventTrigger _currentTarget;
+    private bool _bubbles;
+    private bool _cancelable;
+    private String _types;
+
+    public EventObject(String types, bool bubbles = false, bool cancelable = false)
+    {
+        this._types = types;
+        this._bubbles = bubbles;
+        this._cancelable = cancelable;
+    }
+
+    public EventTrigger target
+    {
+        get { return _target; }
+    }
+
+    internal EventTrigger setTarget
+    {
+        set { _target = value; }
+    }
+
+    public EventTrigger currentTarget
+    {
+        get { return _currentTarget; }
+    }
+
+    internal EventTrigger setCurrentTarget
+    {
+        set { _currentTarget = value; }
+    }
+
+    public int eventPhase
+    {
+        get { return _eventPhase; }
+    }
+
+    internal int setEventPhase
+    {
+        set { _eventPhase = value; }
+    }
+
+    public bool bubbles
+    {
+        get { return _bubbles; }
+    }
+
+    public String types
+    {
+        get { return _types; }
+    }
+
+    public bool cancelable
+    {
+        get { return _cancelable; }
+    }
+}
---
+++ Client/Moba/Assets/Framework/Network/SocketEvent.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class SocketEvent : EventObject
+{
+    /// <summary>
+    /// 连接失败
+    /// </summary>
+    public const string ERROR = "error";
+    /// <summary>
+    /// 连接断开
+    /// </summary>
+    public const string DISCONNECTED = "disconnected";
+    /// <summary>
+    /// 读取数据失败
+    /// </summary>
+    public const string READ_ERROR = "read error";
+    /// <summary>
+    /// 服务器连接成功
+    /// </summary>
+    public const string SERVER_SOCKET_CONNECTED = "server socket connected";
+    /// <summary>
+    /// 服务器登陆成功
+    /// </summary>
+    public const string SERVER_LOGIN_SUCCESS = "mmo server login success";
+    /// <summary>
+    /// 服务器登陆失败
+    /// </summary>
+    public const string SERVER_LOGIN_FAIL = "mmo server login fail";
+    /// <summary>
+    /// 服务器进入成功
+    /// </summary>
+    public const string SERVER_ENTER_SUCCESS = "mmo server enter success";
+    /// <summary>
+    /// 服务器进入失败
+    /// </summary>
+    public const string SERVER_ENTER_FAIL = "mmo server enter fail";
+    /// <summary>
+    /// 服务器初始化成功
+    /// </summary>
+    public const string SERVER_INIT_SUCCESS = "mmo server init success";
+    /// <summary>
+    /// 服务器初始化失败
+    /// </summary>
+    public const string SERVER_INIT_FAIL = "mmo server init fail";
+    /// <summary>
+    /// 踢下线
+    /// </summary>
+    public const string MMO_KICK_PLAYER = "mmo kick player";
+    public SocketEvent(string type)
+        : base(type)
+    {
+
+    }
+    public bool reConnect { get; private set; }
+    public int kickCode { get; private set; }
+    public SocketEvent(string type,bool reConnect)
+        : base(type)
+    {
+        this.reConnect = reConnect;
+    }
+    public SocketEvent(string type, int kickCode)
+        : base(type)
+    {
+        this.kickCode = kickCode;
+    }
+}
