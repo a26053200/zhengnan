ca3ecb843f4419bbc1712906b79ad2d3d5bcf8f1
a26053200 <zhengnan262@qq.com>
2019-08-29 00:41:57 +0800

    BundleManager

--- Client/Framework/Build/build-windows-ab-mode.bat
+++ Client/Framework/Build/build-windows-ab-mode.bat
@@ -1,13 +1,15 @@
 @echo off
 
-set BUILD_TARGET = "-buildTarget windows64"
-set LANGUAGE = "-language zh_CN"
+::set BUILD_TARGET = "-buildTarget windows64"
+::#set LANGUAGE = "-language zh_CN"
 set UNITY_LOG_PATH=%cd%\unity_log.txt
 set UNITY_EDITOR_PATH="D:\Program Files\Unity2018.4.0f1\Editor\Unity.exe"
-set PROJECT_PATH="D:\work\WorkSpace_Unity\mrpg_trunk"
+set PROJECT_PATH=D:\work\WorkSpace_Unity\mrpg_trunk
+set METHOD_NAME=BM.BundlerManager.BuildAssetBundle
 echo Lunch unity.exe,Please wait a moment...
 
-%UNITY_EDITOR_PATH% -batchmode -nographics -logFile %UNITY_LOG_PATH% -projectPath %PROJECT_PATH% -executeMechod BM.BundlerManager.BuildAssetBundle %BUILD_TARGET% %LANGUAGE%
+echo %UNITY_EDITOR_PATH% -batchmode -quit -logFile %UNITY_LOG_PATH% -projectPath %PROJECT_PATH% -executeMethod %METHOD_NAME%
+%UNITY_EDITOR_PATH% -batchmode -quit -logFile %UNITY_LOG_PATH% -projectPath %PROJECT_PATH% -executeMethod %METHOD_NAME%
 
 echo Build Asset Bundle Done
 pause
